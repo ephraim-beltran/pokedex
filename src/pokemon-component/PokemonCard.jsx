6e465acfdec46407f10ecb67039cfb914de4320d
Ephraim Beltran <github@beltran.cc>
2022-10-03 04:27:31 -0400

    Added loading state

--- src/pokemon-component/PokemonCard.jsx
+++ src/pokemon-component/PokemonCard.jsx
@@ -1,40 +1,70 @@
-import { useState , useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./PokemonCard.css";
+import { PuffLoader, BarLoader } from "react-spinners";
 
 const PokemonCard = ({ pokeApi }) => {
-  const [pokeId, setPokeId] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [pokeId, setPokeId] = useState("");
   const [pokeImage, setPokeImage] = useState("");
   const [pokeName, setPokeName] = useState("");
   const [pokeDexNum, setPokeDexNum] = useState("");
   const pokemonData = `/pokemon/${pokeId}`;
 
   useEffect(() => {
+    setLoading(true);
     fetch(pokeApi)
-      .then(res => res.json())
-      .then(species => {
+      .then((res) => res.json())
+      .then((species) => {
         setPokeId(species.id);
         setPokeName(species.name);
-        setPokeDexNum(species.pokedex_numbers[species.pokedex_numbers.findIndex(obj => obj.pokedex.name === 'national')].entry_number)
+        setPokeDexNum(
+          species.pokedex_numbers[
+            species.pokedex_numbers.findIndex(
+              (obj) => obj.pokedex.name === "national"
+            )
+          ].entry_number
+        );
 
         fetch(`https://pokeapi.co/api/v2/pokemon/${pokeId}`)
-        .then(res => res.json())
-        .then(pokemon => {
-          if (pokemon.sprites){
-            setPokeImage(pokemon.sprites.other["official-artwork"]["front_default"]);
-          }
-        })
-        
+          .then((res) => res.json())
+          .then((pokemon) => {
+            if (pokemon.sprites) {
+              setPokeImage(
+                pokemon.sprites.other["official-artwork"]["front_default"]
+              );
+              setLoading(false);
+            }
+          });
       });
   }, [pokeId, pokeApi]);
 
   return (
     <Link to={pokemonData} className="card mx-1" style={{ width: "10rem" }}>
-      <img src={pokeImage} className="card-img-top" alt='' role='presentation' loading='lazy' />
-      <div className="card-body">
-        <h5 className="card-title">{pokeName}</h5>
-        <div className="card-subtitle">#{pokeDexNum}</div>
-      </div>
+      {loading ? (
+        <PuffLoader color="#000000" />
+      ) : (
+          <img
+            src={pokeImage}
+            className="card-img-top"
+            alt=""
+            role="presentation"
+            loading="lazy"
+          />
+          )
+      }
+          <div className="card-body">
+          {
+            loading ?
+            <BarLoader color="#000000" />
+            :
+            <>
+            <h5 className="card-title">{pokeName}</h5>
+            <div className="card-subtitle">#{pokeDexNum}</div>
+            </>
+
+          }
+          </div>
     </Link>
   );
 };

5e1045fd263e9671286828b39cc11216ae14fc84
Ephraim Beltran <github@beltran.cc>
2022-10-22 01:08:00 -0400

    Reworked fetch request for simpicity

--- src/pokemon-component/PokemonCard.jsx
+++ src/pokemon-component/PokemonCard.jsx
@@ -4,69 +4,71 @@ import "./PokemonCard.css";
 import { PuffLoader, BarLoader } from "react-spinners";
 
 const PokemonCard = ({ pokeApi }) => {
-  const [loading, setLoading] = useState(false);
-  const [pokeId, setPokeId] = useState("");
-  const [pokeImage, setPokeImage] = useState("");
-  const [pokeName, setPokeName] = useState("");
-  const [pokeDexNum, setPokeDexNum] = useState("");
-  const pokemonData = `/pokemon/${pokeId}`;
+  const [loading, setLoading] = useState(true);
+  const [listCard, setListCard] = useState({});
+  const pokemonData = `/pokemon/${listCard.id}`;
 
   useEffect(() => {
     const controller = new AbortController();
-    setLoading(true);
-    fetch(pokeApi)
-      .then((res) => res.json())
-      .then((species) => {
-        setPokeId(species.id);
-        setPokeName(species.name);
-        setPokeDexNum(
-          species.pokedex_numbers[
-            species.pokedex_numbers.findIndex(
-              (obj) => obj.pokedex.name === "national"
-            )
-          ].entry_number
-        );
+    // setLoading(true);
 
-        fetch(`https://pokeapi.co/api/v2/pokemon/${pokeId}`, { signal: controller.signal })
-          .then((res) => res.json())
-          .then((pokemon) => {
-            if (pokemon.sprites) {
-              setPokeImage(
-                pokemon.sprites.other["official-artwork"]["front_default"]
-              );
-              setLoading(false);
-            }
-          });
+    const fetchListItem = async () => {
+      const cardData = await fetch(pokeApi, { signal: controller.signal }).then(
+        (res) => res.json()
+      );
+
+      const cardImage = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${cardData.id}`,
+        { signal: controller.signal }
+      ).then((res) => res.json());
+
+      const pokedex_number =
+        cardData.pokedex_numbers[
+          cardData.pokedex_numbers.findIndex(
+            (obj) => obj.pokedex.name === "national"
+          )
+        ].entry_number;
+
+      const image =
+        cardImage.sprites.other["official-artwork"]["front_default"];
+
+      setListCard({
+        name: cardData.name,
+        id: cardData.id,
+        pokedex_number: pokedex_number,
+        image: image,
       });
-      return () => { controller.abort() }
-  }, [pokeId, pokeApi]);
+    };
+    pokeApi && fetchListItem();
+    Object.keys(listCard).length > 0 && setLoading(false)
+
+    return () => {
+      controller.abort();
+    };
+  }, [pokeApi, listCard]);
 
   return (
     <Link to={pokemonData} className="card mx-1" style={{ width: "10rem" }}>
       {loading ? (
         <PuffLoader color="#000000" />
       ) : (
-          <img
-            src={pokeImage}
-            className="card-img-top"
-            alt=""
-            role="presentation"
-            loading="lazy"
-          />
-          )
-      }
-          <div className="card-body">
-          {
-            loading ?
-            <BarLoader color="#000000" />
-            :
-            <>
-            <h5 className="card-title">{pokeName}</h5>
-            <div className="card-subtitle">#{pokeDexNum}</div>
-            </>
-
-          }
-          </div>
+        <img
+          src={listCard.image}
+          className="card-img-top"
+          alt=""
+          role="presentation"
+        />
+      )}
+      <div className="card-body">
+        {loading ? (
+          <BarLoader color="#000000" />
+        ) : (
+          <>
+            <h5 className="card-title">{listCard.name}</h5>
+            <div className="card-subtitle">#{listCard.pokedex_number}</div>
+          </>
+        )}
+      </div>
     </Link>
   );
 };

245abea1ffdd28c1823f2e6ca2a3dd79f75e8379
Ephraim Beltran <github@beltran.cc>
2022-10-21 22:40:59 -0400

    Added loader styling

--- src/pages/PokemonData/PokemonTabContent.jsx
+++ src/pages/PokemonData/PokemonTabContent.jsx
@@ -1,37 +1,78 @@
-const PokemonTabContent = ({activeForm}) => {
-    
+import { useEffect } from "react";
+import { MoonLoader } from "react-spinners";
+import "./PokemonTabContent.css";
+
+const PokemonTabContent = ({
+  activeForm,
+  pokemonInfo
+}) => {
+  // =====================
+  // For console debugging
+  // =====================
+    useEffect(() => {
+      Object.keys(activeForm).length > 0
+      ? console.log('Active form selected')
+      : console.warn('No form active');
+    }, [activeForm])
+  // =====================
+
+  if (Object.keys(activeForm).length > 0) {
+    const activeName = activeForm.pokemon.name;
     return (
-        <>
-        <div
-          className="tab-pane fade"
-          id="home-tab-pane"
+      <div
+          className="tab-pane fade show active"
+          id={`${activeName}-tab-pane`}
           role="tabpanel"
-          aria-labelledby="home-tab"
+          aria-labelledby={`${activeName}-tab`}
           tabIndex="0"
         >
-          This is home
+          <p>Pokemon loaded: {pokemonInfo.name}</p>
+          <p>Form loaded: {activeForm.pokemon.name}</p>
         </div>
-        <div
-          // className="tab-pane fade show active"
-          className="tab-pane fade show"
-          id="charizard-mega-x-tab-pane"
-          role="tabpanel"
-          aria-labelledby="charizard-mega-x-tab"
-          tabIndex="0"
-        >
-          This is profile
-        </div>
-        <div
-          className="tab-pane fade"
-          id="contact-tab-pane"
-          role="tabpanel"
-          aria-labelledby="contact-tab"
-          tabIndex="0"
-        >
-          This is contacts
-        </div>
-        </>
     )
+  } else {
+    return (
+      <div className="loader-container">
+        <MoonLoader color="hsla(168, 0%, 0%, 1)" />
+      </div>
+    )
+  }
+
+
+
+
+    // return (
+    //     <>
+    //     <div
+    //       className="tab-pane fade"
+    //       id="home-tab-pane"
+    //       role="tabpanel"
+    //       aria-labelledby="home-tab"
+    //       tabIndex="0"
+    //     >
+    //       This is home
+    //     </div>
+    //     <div
+    //       className="tab-pane fade show active"
+    //       className="tab-pane fade show"
+    //       id="charizard-mega-x-tab-pane"
+    //       role="tabpanel"
+    //       aria-labelledby="charizard-mega-x-tab"
+    //       tabIndex="0"
+    //     >
+    //       This is profile
+    //     </div>
+    //     <div
+    //       className="tab-pane fade"
+    //       id="contact-tab-pane"
+    //       role="tabpanel"
+    //       aria-labelledby="contact-tab"
+    //       tabIndex="0"
+    //     >
+    //       This is contacts
+    //     </div>
+    //     </>
+    // )
  }
 
  export default PokemonTabContent;

beeafc3d9b64f9372b5da6929be0cfa248bd874c
Ephraim Beltran <github@beltran.cc>
2022-10-27 21:41:32 -0400

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { HashRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter basename='/pokedex'>
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>
